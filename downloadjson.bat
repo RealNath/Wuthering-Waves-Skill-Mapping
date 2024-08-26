ee949b87204d7890536f7da56cd67451e0d9b730
Nathan <therealnathanmc7383@gmail.com>
2024-08-27 01:52:12 +0700

    revised variables

--- downloadjson.bat
+++ downloadjson.bat
@@ -1,13 +1,13 @@
 @echo off
 
 set url1=https://raw.githubusercontent.com/Dimbreath/WutheringData/master/TextMap/en/MultiText.json
-set output_file1=MultiText.json
+set output1=MultiText.json
 
 set url2=https://raw.githubusercontent.com/Dimbreath/WutheringData/master/ConfigDB/Skill.json
-set output_file2=Skill.json
+set output2=Skill.json
 
-curl -L -o %output_file1% %url1%
-curl -L -o %output_file2% %url2%
+curl -L -o %output1% %url1%
+curl -L -o %output2% %url2%
 
-echo Files downloaded: %output_file1%, %output_file2%
+echo Files downloaded: %output1%, %output2%
 pause
